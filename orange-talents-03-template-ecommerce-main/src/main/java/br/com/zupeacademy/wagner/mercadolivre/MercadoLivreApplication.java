84b49442b27607e0e8830139256ea28cab3ae8b5
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-04-19 14:11:39 -0300

    teste

--- orange-talents-03-template-ecommerce-main/src/main/java/br/com/zupeacademy/wagner/mercadolivre/MercadoLivreApplication.java
+++ orange-talents-03-template-ecommerce-main/src/main/java/br/com/zupeacademy/wagner/mercadolivre/MercadoLivreApplication.java
@@ -3,8 +3,6 @@ package br.com.zupeacademy.wagner.mercadolivre;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 public class MercadoLivreApplication {
 
