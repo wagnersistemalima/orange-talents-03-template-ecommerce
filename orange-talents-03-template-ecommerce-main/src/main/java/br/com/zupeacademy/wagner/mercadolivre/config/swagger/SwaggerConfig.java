4dc4d88c3b799872599636d25279cdd2d6f352e5
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-04-19 14:10:24 -0300

    update config swagger

--- orange-talents-03-template-ecommerce-main/src/main/java/br/com/zupeacademy/wagner/mercadolivre/config/swagger/SwaggerConfig.java
+++ orange-talents-03-template-ecommerce-main/src/main/java/br/com/zupeacademy/wagner/mercadolivre/config/swagger/SwaggerConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zupeacademy.wagner.mercadolivre.usuario.Usuario;
 import springfox.documentation.builders.ParameterBuilder;
@@ -12,27 +13,31 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
+@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 	@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.zupeacademy.wagner.mercadolivre"))
-                .paths(PathSelectors.ant("/**"))
-                .build()
-                .ignoredParameterTypes(Usuario.class)
-                .globalOperationParameters(
-                        Arrays.asList(
-                                new ParameterBuilder()
-                                    .name("Authorization")
-                                    .description("Header para Token JWT")
-                                    .modelRef(new ModelRef("string"))
-                                    .parameterType("header")
-                                    .required(false)
-                                    .build()));
-                
-    }
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.paths(PathSelectors.any())
+				.build()
+				 .ignoredParameterTypes(Usuario.class)
+	                .globalOperationParameters(
+	                        Arrays.asList(
+	                                new ParameterBuilder()
+	                                    .name("Authorization")
+	                                    .description("Header para Token JWT")
+	                                    .modelRef(new ModelRef("string"))
+	                                    .parameterType("header")
+	                                    .required(false)
+	                                    .build()));          
+
+	}
 }
+
+
