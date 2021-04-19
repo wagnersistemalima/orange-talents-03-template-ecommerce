7c5d3d18cea88e4669449801acd4f9cfe3c44353
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-04-19 12:16:08 -0300

    Swagger configuracao parte 2

--- orange-talents-03-template-ecommerce-main/src/main/java/br/com/zupeacademy/wagner/mercadolivre/config/swagger/SwaggerConfig.java
+++ orange-talents-03-template-ecommerce-main/src/main/java/br/com/zupeacademy/wagner/mercadolivre/config/swagger/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package br.com.zupeacademy.wagner.mercadolivre.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zupeacademy.wagner.mercadolivre.usuario.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,8 +20,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
-				.build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.paths(PathSelectors.any())
+				.build()
+				 .ignoredParameterTypes(Usuario.class)
+	                .globalOperationParameters(
+	                        Arrays.asList(
+	                                new ParameterBuilder()
+	                                    .name("Authorization")
+	                                    .description("Header para Token JWT")
+	                                    .modelRef(new ModelRef("string"))
+	                                    .parameterType("header")
+	                                    .required(false)
+	                                    .build()));          
+
 	}
 }
